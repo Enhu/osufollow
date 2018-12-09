f47e8738e18172181014a7655b76872a5322a9e1
Martin Lorenzo <martin.rodrigo.lorenzo@gmail.com>
2018-12-09 16:23:16 -0300

    Finished register and login html and added global variables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {Globals} from './globals';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Globals]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private role: string;
+  private loginInfo: boolean;
+
+  constructor(private globals: Globals) {
+    this.role = globals.role;
+    this.loginInfo = globals.loginInfo;
+  }
+
+  ngOnInit(){
+   console.log(this.role);
+
+
+
+  }
 
 }
