cbfc0bfbc17b0748e00c19095c4e9798a3391b97
martinlorenzo <martinlorenzo@users.noreply.github.com>
2018-12-11 17:41:12 -0300

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Globals} from './globals';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,12 +12,23 @@ export class AppComponent implements OnInit {
   private role: string;
   private loginInfo: [String, number];
 
-  constructor(private globals: Globals) {
+  constructor(private globals: Globals, private router:Router) {
     this.role = globals.role;
     this.loginInfo = globals.loginInfo;
+  }
+
+  
+  value = '';
+  onEnter(value: string) { 
+    this.value = value;
+    this.goStats();
+  }
+  goStats(){
+    this.router.navigate(['/stats'], { queryParams: { id: this.value}});
   }
 
   ngOnInit(){
     console.log(this.role);  
   }
+
 }
