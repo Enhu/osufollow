83a1a5ad962403455dc7bb0c15152247685e558c
Martin Lorenzo <martin.rodrigo.lorenzo@gmail.com>
2018-12-17 17:37:12 -0300

    added test to the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { ProfileComponent }     from './profile/profile.component';
 import { LoginComponent} from "./login/login.component";
 import { StatsComponent } from './stats/stats.component';
 import { AuthGuard  } from './_helpers/auth.guard';
-
+import { TestComponent } from './test/test.component';
 const routes: Routes = [
   { path: 'register', component: RegisterComponent},
   { path: 'main', component: MainComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
+  { path: 'test', component: TestComponent },
   { path: 'stats', component: StatsComponent }
 ];
 
